bc5ca4b74c166978961c36f5016edc8ef36996f5
jay <jaypifiverr@gmail.com>
2022-06-28 14:50:14 -0400

    commit tree 1d4104c98c6c0db5ba4d8e5a447c55eac3e5a4ba

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
 
-// Write your code here
+console.log(multiply)
+
+//////
+
+const random = (Math.floor(Math.random() + 1))
+
+console.log(random);
+
+//////
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4;
+
+console.log(mod)
+
+//////
+
+const max = Math.max(4, 2, 8, 12, 16, 20)
+
+console.log(max)
